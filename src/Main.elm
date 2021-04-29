8751a479e42d25d2dbd8eb8247e8573615c3ee12
Damola <dshomoye@gmail.com>
2021-04-29 16:52:20 -0700

    zero to a hunn'd

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,144 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
 import Browser
-import Css exposing (..)
+import Css exposing (auto, displayFlex, margin, padding, px, vw, width)
+import Html
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css)
-import Html.Styled.Events exposing (onClick)
+import Html.Styled.Attributes exposing (name, src, type_, value)
+import Html.Styled.Events exposing (onClick, onInput, onSubmit)
+import Http
+import Json.Decode exposing (Decoder, field, list, string)
+import List exposing (filter, sort)
+import Random
+import Random.List
 
 
 
--- elements
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+
+
+
+-- MODEL
+
+
+type alias GameState =
+    { words : List String
+    , enteredWords : List String
+    , magicWord : String
+    , guess : String
+    }
+
+
+type Model
+    = Loaded GameState
+    | Loading
+    | Error
+    | Won GameState
+
+
+defaultModel : Model
+defaultModel =
+    Loaded (GameState [] [] "" "")
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading, getWords )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GotWords (Result Http.Error (List String))
+    | GotMagicWord ( Maybe String, List String )
+    | Guess String
+    | CheckGuess
+    | Reset
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotWords result ->
+            case result of
+                Ok words ->
+                    ( Loaded (GameState (List.map String.toLower words) [] "" ""), randomWord words )
+
+                Err _ ->
+                    ( defaultModel, Cmd.none )
+
+        GotMagicWord ( mWord, _ ) ->
+            case mWord of
+                Just word ->
+                    case model of
+                        Loaded oldState ->
+                            ( Loaded { oldState | magicWord = word }, Cmd.none )
+
+                        Won oldState ->
+                            ( Won oldState, Cmd.none )
+
+                        _ ->
+                            ( defaultModel, Cmd.none )
+
+                Nothing ->
+                    ( defaultModel, Cmd.none )
+
+        Guess g ->
+            case model of
+                Loaded oldState ->
+                    ( Loaded { oldState | guess = g }, Cmd.none )
+
+                Won oldState ->
+                    ( Won oldState, Cmd.none )
+
+                _ ->
+                    ( defaultModel, Cmd.none )
+
+        CheckGuess ->
+            case model of
+                Loaded oldState ->
+                    if String.toLower oldState.magicWord == String.toLower oldState.guess then
+                        ( Won oldState, Cmd.none )
+
+                    else if List.member (String.toLower oldState.guess) oldState.words && not (List.member (String.toLower oldState.guess) oldState.enteredWords) then
+                        ( Loaded { oldState | enteredWords = oldState.enteredWords ++ [ String.toLower oldState.guess ], guess = "" }, Cmd.none )
+
+                    else
+                        ( Loaded oldState, Cmd.none )
+
+                Won oldState ->
+                    ( Won oldState, Cmd.none )
+
+                _ ->
+                    ( defaultModel, Cmd.none )
+
+        Reset ->
+            case model of
+                Loaded oldState ->
+                    ( Loaded oldState, Cmd.none )
+
+                Won oldState ->
+                    ( Loaded { oldState | guess = "" }, randomWord oldState.words )
+
+                _ ->
+                    ( defaultModel, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
 
 container : List (Attribute msg) -> List (Html msg) -> Html msg
 container =
@@ -24,6 +148,7 @@ container =
         ]
 
 
+app : List (Attribute msg) -> List (Html msg) -> Html msg
 app =
     styled div
         [ width (vw 100)
@@ -31,67 +156,83 @@ app =
         ]
 
 
-
--- MAIN
-
-
-main : Program () Model Msg
-main =
-    Browser.document { init = init, update = update, view = view, subscriptions = subscriptions }
+renderGuess : String -> Html msg
+renderGuess w =
+    p [] [ text w ]
 
 
-
--- MODEL
-
-
-type alias Model =
-    Int
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( 0, Cmd.none )
-
-
-
--- UPDATE
-
-
-type Msg
-    = Increment
-    | Decrement
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
-
-        Decrement ->
-            ( model - 1, Cmd.none )
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
--- VIEW
-
-
-view : Model -> Browser.Document Msg
+view : Model -> Html.Html Msg
 view model =
     let
         body =
-            app []
-                [ container []
-                    [ button [ onClick Decrement ] [ text "-" ]
-                    , div [] [ text (String.fromInt model) ]
-                    , button [ onClick Increment ] [ text "+" ]
-                    ]
-                ]
+            case model of
+                Loaded state ->
+                    app []
+                        [ container []
+                            [ h1 [] [ text "Guess the word" ]
+                            , div [] (List.map renderGuess (beforeWords state))
+                            , form [ name "guessword", onSubmit CheckGuess ]
+                                [ input [ onInput Guess, value state.guess ] []
+                                , input [ type_ "submit" ] []
+                                ]
+                            , div [] (List.map renderGuess (afterWords state))
+                            , button [ onClick Reset ] [ text "Restart" ]
+
+                            -- , p []  [text state.magicWord]
+                            ]
+                        ]
+
+                Loading ->
+                    app []
+                        [ container [] [ h2 [] [ text "Loading" ] ] ]
+
+                Error ->
+                    app []
+                        [ container [] [ h2 [] [ text "Error Loading! Try refreshing the page" ] ] ]
+
+                Won _ ->
+                    app []
+                        [ container []
+                            [ div []
+                                [ h2 [] [ text "You Win!" ]
+                                , img [ src "https://source.unsplash.com/random", Html.Styled.Attributes.width 300, Html.Styled.Attributes.height 300 ] []
+                                , div [] [ button [ onClick Reset ] [ text "Restart" ] ]
+                                ]
+                            ]
+                        ]
     in
-    { body = [ toUnstyled body ]
-    , title = "Guess The Word"
-    }
+    toUnstyled body
+
+
+
+--- HELPERS
+
+
+wordsDecoder : Decoder (List String)
+wordsDecoder =
+    field "all_words" (list string)
+
+
+beforeWords : GameState -> List String
+beforeWords m =
+    filter (\n -> String.toLower n < String.toLower m.magicWord) m.enteredWords
+        |> sort
+
+
+afterWords : GameState -> List String
+afterWords m =
+    filter (\n -> String.toLower n > String.toLower m.magicWord) m.enteredWords
+        |> sort
+
+
+getWords : Cmd Msg
+getWords =
+    Http.get
+        { url = "/src/words.json"
+        , expect = Http.expectJson GotWords wordsDecoder
+        }
+
+
+randomWord : List String -> Cmd Msg
+randomWord words =
+    Random.generate GotMagicWord (Random.List.choose words)
